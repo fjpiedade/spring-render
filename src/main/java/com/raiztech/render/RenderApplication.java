f0ee7473cc67130b7aeacb06d4b7f9dbf8d91c9b
fjpiedade <fernandp.joao@gmail.com>
2023-08-02 14:01:49 +0100

    add files on the stage - first commit

---
+++ src/main/java/com/raiztech/render/RenderApplication.java
@@ -0,0 +1,20 @@
+package com.raiztech.render;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class RenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenderApplication.class, args);
+	}
+
+	@GetMapping
+	public String message(){
+		return "Hello Render Deploy Service ...";
+	}
+}
